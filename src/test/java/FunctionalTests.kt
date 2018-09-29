6c6a299805df7bc5fd5f783c83e8cf06de84aa8c
Test User <testuser@tests-mbp.home>
2018-09-29 21:58:40 +0100

    add logic to start mock server into @BeforeClass

--- src/test/java/FunctionalTests.kt
+++ src/test/java/FunctionalTests.kt
@@ -8,6 +8,15 @@ open class FunctionalTest {
 
         @BeforeClass @JvmStatic
         fun setUp() {
+            startWireMock()
+            setupRestAssuredDefaults()
+        }
+
+        private fun startWireMock() {
+            Runtime.getRuntime().exec("java -jar wiremock-standalone-2.19.0.jar --port 8888 --root-dir src/test/resources")
+        }
+
+        private fun setupRestAssuredDefaults() {
             RestAssured.baseURI = "http://localhost"
             RestAssured.port = 8888
             RestAssured.basePath = "/"
